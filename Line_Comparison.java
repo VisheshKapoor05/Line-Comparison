1ade181c57c58e0dd6eb3cc701c08eeba4f0a1cc
VisheshKapoor05 <vishesh17kapoor@gmail.com>
2021-11-12 21:06:40 +0530

    UC4: Main method file

--- Line_Comparison.java
+++ Line_Comparison.java
@@ -1,55 +1,44 @@
+package Line_Comparison;
 import java.util.Scanner;
 
 public class Line_Comparison {
-	public static void main(String[] args)
-	{
+	
+	public static void main(String[] args){
+		
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
 
-	Scanner sc = new Scanner(System.in);
+		//First Line
+		System.out.print("First Coordinate of Line 1, X1: ");
+		double x11 = sc.nextDouble();
+		System.out.print("First Coordinate of Line 1, y1: ");
+		double y11 = sc.nextDouble();
+		System.out.print("Second Coordinate of Line 1, X2: ");
+		double x12 = sc.nextDouble();
+		System.out.print("Second Coordinate of Line 1, y2: ");
+		double y12 = sc.nextDouble();
 
-	//First Line
-	System.out.print("First Coordinate of Line 1, X1: ");
-	double x11 = sc.nextDouble();
-	System.out.print("First Coordinate of Line 1, y1: ");
-	double y11 = sc.nextDouble();
-	System.out.print("Second Coordinate of Line 1, X2: ");
-	double x12 = sc.nextDouble();
-	System.out.print("Second Coordinate of Line 1, y2: ");
-	double y12 = sc.nextDouble();
+		//Second Line
+		System.out.print("First Coordinate of Line 2, X1: ");
+		double x21 = sc.nextDouble();
+		System.out.print("First Coordinate of Line 2, y1: ");
+		double y21 = sc.nextDouble();
+		System.out.print("Second Coordinate of Line 2, X2: ");
+		double x22 = sc.nextDouble();
+		System.out.print("Second Coordinate of Line 2, y2: ");
+		double y22 = sc.nextDouble();		
 
-	//Second Line
-	System.out.print("First Coordinate of Line 2, X1: ");
-	double x21 = sc.nextDouble();
-	System.out.print("First Coordinate of Line 2, y1: ");
-	double y21 = sc.nextDouble();
-	System.out.print("Second Coordinate of Line 2, X2: ");
-	double x22 = sc.nextDouble();
-	System.out.print("Second Coordinate of Line 2, y2: ");
-	double y22 = sc.nextDouble();
-
-	double line1 = linelength(x11, x12, y11, y12);
-	double line2 = linelength(x21, x22, y21, y22);
-	System.out.println("length of the line 1 = " +line1);
-	System.out.println("length of the line 2 = " +line2);
-
-	/* Its showing error: double cannot be dereferenced
-	if( line1.equals(line2) )
-	 System.out.println("The 2 lines are equal in length");
-	else
- 	 System.out.println("The 2 lines are  not equal in length");
-	*/
-
-	if( line1 == line2 )
-	 System.out.println("The 2 lines are equal in length");
-	else
- 	 System.out.println("The 2 lines are  not equal in length");
-  	}
-
-	static double linelength(double x1, double x2, double y1, double y2)
-	{
-	double X = Math.pow((x2-x1),2);
-	double Y = Math.pow((y2-y1),2);
-	double Length = Math.floor(Math.sqrt(X+Y));
-	return Length;
+		computation_of_lines line1 = new computation_of_lines(x11, x12, y11, y12);
+		computation_of_lines line2 = new computation_of_lines(x21, x22, y21, y22);
+		double linelength_1 = line1.linelength();
+		double linelength_2 = line2.linelength();
+		System.out.println("length of the line 1 = " +linelength_1);
+		System.out.println("length of the line 2 = " +linelength_2);
+		
+		actions_on_lines lines = new actions_on_lines(linelength_1, linelength_2);
+		
+		lines.compareTo_method();
+		lines.equalsTo_method();
 	}
 
 }
